661b84b3c76600c5825f910faa29c125536a4fd8
Mariana Niño Solano <mnino499@unab.edu.co>
2026-09-22 19:29:17 -0500

    Modelos

---
+++ app/Models/CartItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    protected $table = 'card_items';
+    protected $primaryKey = 'id';
+}
