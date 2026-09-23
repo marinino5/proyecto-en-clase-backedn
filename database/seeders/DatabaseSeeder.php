e209c7ad70cd49c9456b273e4a3a2cf37335aa43
Mariana Niño Solano <mnino499@unab.edu.co>
2026-09-22 19:33:15 -0500

    Factory Category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        /*
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        */
+
+        $category1 = new Category();
+        $category1->name = "Patinetas eléctricas";
+        $category1->description = "Vehículos eléctricos ágiles para recorridos urbanos de corta distancia.";
+        $category1->save();
+
+        $category2 = new Category();
+        $category2->name = "Bicicletas eléctricas";
+        $category2->description = "Vehículos eléctricos cómodos para recorridos urbanos de media distancia.";
+        $category2->save();
+
+        Category::factory(10)->create();
     }
 }
